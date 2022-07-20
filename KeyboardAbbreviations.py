cc554d85dcd81c0954d9fe0b39419778fd36f7b6
Neerosh <torgash.wraith@gmail.com>
2022-07-20 14:30:00 -0300

    Renamed: KeyboardShortcuts to KeyboardAbbreviations

--- KeyboardAbbreviations.py
+++ KeyboardAbbreviations.py
@@ -2,12 +2,12 @@ import keyboard,time
 from threading import Thread
 
 def CheckAbbreviation():
-    shortcuts = {
+    abbreviations = {
         "neerosh-github" : "https://github.com/Neerosh",
         "neerosh-site"   : "https://neerosh.github.io/"
     }
-    for shortcut, fullText in shortcuts.items():
-        keyboard.add_abbreviation(shortcut, fullText)
+    for abbreviation, fullText in abbreviations.items():
+        keyboard.add_abbreviation(abbreviation, fullText)
         # use space after typing the abbreviation to make python replace the text
     while True:  
         time.sleep(1)
